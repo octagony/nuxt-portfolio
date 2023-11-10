307704e01101ae2f98a9206e42e07219bbd34b35
octagony <moctagony@gmail.com>
2023-11-10 13:51:25 +0300

    [FEAT] Add skills component

---
+++ components/Skills.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="grid grid-cols-12 gap-2 mt-2">
+    <div class="col-span-4 h-[200px] bg-primary rounded-xl p-4">
+      <div class="grid grid-cols-4 gap-1 place-items-center">
+        <div>
+          <Icon name="devicon:react" />
+        </div>
+        <div>
+          <Icon name="devicon:vuejs" />
+        </div>
+        <div>
+          <Icon name="logos:nextjs-icon" />
+        </div>
+        <div>
+          <Icon name="devicon:nuxtjs" />
+        </div>
+        <div>
+          <Icon name="logos:typescript-icon" />
+        </div>
+        <div>
+          <Icon name="devicon:tailwindcss" />
+        </div>
+        <div>
+          <Icon name="vscode-icons:file-type-astro" />
+        </div>
+        <div>
+          <Icon name="logos:pinia" />
+        </div>
+        <div>
+          <Icon name="logos:redux" />
+        </div>
+        <div>
+          <Icon name="vscode-icons:file-type-firebase" />
+        </div>
+        <div>
+          <Icon name="devicon:reactrouter" />
+        </div>
+        <div>
+          <Icon name="devicon:jquery" />
+        </div>
+      </div>
+    </div>
+    <div class="col-span-8 h-[200px] bg-primary rounded-xl p-4 text-center">
+      <p class="mt-2">
+        as a developer, i like to implement
+        <span class="text-blue-300">new technologies</span> into my projects
+      </p>
+    </div>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,5 @@
 <template>
   <TopCards />
   <Contacts />
+  <Skills />
 </template>
