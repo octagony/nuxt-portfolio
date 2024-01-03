8a4ca00d5128d963674686dd32b3253c2cfa3c3a
octagony <moctagony@gmail.com>
2024-01-03 22:11:14 +0300

    [FEAT] Added responsive layout for project page

--- pages/projects/[id].vue
+++ pages/projects/[id].vue
@@ -23,41 +23,57 @@ const project =
         <button class="btn btn-primary rounded-xl text-xl">Go back</button>
       </NuxtLink>
       <div class="grid grid-cols-12 gap-2 mt-2">
-        <div class="col-span-6 h-[350px] bg-primary rounded-xl p-4">
+        <div
+          class="col-span-12 md:col-span-6 md:h-[350px] bg-primary rounded-xl p-4"
+        >
           <img
             :src="project?.img"
             :alt="project?.description"
             class="w-full h-full rounded-xl"
           />
         </div>
-        <div class="col-span-6 h-[350px] bg-primary rounded-xl p-4">
-          <div class="mt-8">
-            <h1 class="text-blue-300 text-6xl">{{ project?.title }}</h1>
-            <p class="mt-4">{{ project?.subtitle }}</p>
+        <div
+          class="col-span-12 md:col-span-6 md:h-[350px] bg-primary rounded-xl p-4"
+        >
+          <div class="my-4">
+            <h1 class="text-blue-300 text-5xl md:text-6xl">
+              {{ project?.title }}
+            </h1>
+            <p class="mt-4 text-3xl md:text-4xl">{{ project?.subtitle }}</p>
           </div>
         </div>
-        <div class="col-span-12 h-[150px] bg-primary rounded-xl p-4">
+        <div class="col-span-12 bg-primary rounded-xl p-4">
           <p class="text-center mb-6">builded with:</p>
-          <ul class="h-full text-2xl flex flex-wrap justify-around mt-4">
-            <li v-for="tool in project?.tools" :key="tool">
+          <ul
+            class="text-2xl md:text-2xl grid gap-8 grid-cols-2 place-items-center md:flex md:flex-wrap md:justify-around mt-4"
+          >
+            <li
+              v-for="tool in project?.tools"
+              :key="tool"
+              class="[&:nth-child(7)]:col-span-full"
+            >
               <span class="text-primary bg-blue-300 p-2 rounded-xl">{{
                 tool
               }}</span>
             </li>
           </ul>
         </div>
-        <div class="col-span-8 h-[200px] bg-primary rounded-xl p-4">
-          <p class="text-2xl font-medium">{{ project?.description }}</p>
+        <div
+          class="col-span-12 lg:col-span-8 md:h-[200px] bg-primary rounded-xl p-4"
+        >
+          <p class="text-2xl font-bold">{{ project?.description }}</p>
         </div>
-        <div class="col-span-4 h-[200px] bg-primary rounded-xl p-4">
+        <div
+          class="col-span-12 lg:h-[200px] bg-primary rounded-xl p-4 text-center place-items-center lg:col-span-4"
+        >
           <a
             :href="project?.githubLink"
-            class="btn bg-blue-300 text-primary text-xl rounded-xl hover:bg-primary hover:text-blue-300"
-            ><Icon name="uil:github" />Link to project on Github</a
+            class="btn bg-blue-300 flex items-center text-primary text-xl rounded-xl hover:bg-primary hover:text-blue-300 mx-auto md:max-w-max lg:mx-0"
+            ><Icon name="uil:github" />Link to Github</a
           >
           <a
             :href="project?.websiteLink"
-            class="btn bg-blue-300 text-primary text-xl rounded-xl hover:bg-primary hover:text-blue-300 mt-8"
+            class="btn bg-blue-300 flex items-center text-primary text-xl rounded-xl hover:bg-primary hover:text-blue-300 mt-8 mx-auto md:max-w-max lg:mx-0"
             ><Icon name="gg:website" />Link to website project</a
           >
         </div>
